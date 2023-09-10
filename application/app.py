198a1460eff16c15c9e38d97bb42aad7552c0882
Amit Sharma <amit.sharma8@natwest.com>
2023-09-10 13:56:51 +0100

    updating CF script

--- application/app.py
+++ application/app.py
@@ -1,39 +1,49 @@
-from flask import Flask
-from flask import json
 import logging
+from flask import Flask, json
 
 app = Flask(__name__)
 
+# Type hinting for app.logger
+app.logger: logging.Logger
+
 @app.route('/status')
 def healthcheck():
+    """
+    Endpoint to check the health status.
+    """
     response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
     )
 
-    app.logger.info('Status request successfull')
+    app.logger.info('Status request successful')
     return response
 
 @app.route('/metrics')
 def metrics():
+    """
+    Endpoint to provide metrics data.
+    """
     response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
-            status=200,
-            mimetype='application/json'
+        response=json.dumps({"status": "success", "code": 0, "data": {"UserCount": 140, "UserCountActive": 23}}),
+        status=200,
+        mimetype='application/json'
     )
 
-    app.logger.info('Metrics request successfull')
+    app.logger.info('Metrics request successful')
     return response
 
-@app.route("/")
+@app.route('/')
 def hello():
-    app.logger.info('Main request successfull')
-
+    """
+    Default endpoint to greet the world.
+    """
+    app.logger.info('Main request successful')
     return "Hello World!"
 
 if __name__ == "__main__":
-    ## stream logs to a file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    
+    # Stream logs to a file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
     app.run(host='0.0.0.0')
